16ef03d332c8d971ed96ac30a5274a0a4b89c1b7
zhangdi <1258956799@qq.com>
2023-11-06 15:59:45 +0800

    chore:test

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,11 @@
 // eslint.config.js
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  // Enable stylistic formatting rules
-  // stylistic: true,
-
-  // Or customize the stylistic rules
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: 'single', // or 'double'
+export default antfu(
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      // 'no-console': 'off'
+    },
   },
-  
-  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
-  typescript: true,
-  vue: true,
-
-  // Disable jsonc and yaml support
-  jsonc: false,
-  yaml: false,
-
-  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
-  ignores: [
-    // ...globs
-  ]
-})
+)
